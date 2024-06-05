1f9bebff93b3453a9f8d47de1a324cb3fd0c5e23
TrietHuynh443 <triethuynh4403@gmail.com>
2024-06-05 15:03:41 +0700

    add project structure

---
+++ lib/GUI/gui_history.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class GUIHistory extends StatefulWidget{
+  const GUIHistory({super.key});
+
+  @override
+  State<StatefulWidget> createState() => GUIHistoryState();
+
+}
+
+class GUIHistoryState extends State{
+
+  @override
+  Widget build(BuildContext context) {
+      return Container();
+  }
+}
