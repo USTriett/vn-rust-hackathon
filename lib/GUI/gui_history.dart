1479b1110686d4f436533bc633e97c4ebc9166ef
NTTVy03 <trucvy05062003@gmail.com>
2024-06-05 20:07:32 +0700

    feat: metamask - bug intent

--- lib/GUI/gui_history.dart
+++ lib/GUI/gui_history.dart
@@ -1,19 +1,15 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-class GUIHistory extends StatefulWidget{
+class GUIHistory extends StatefulWidget {
   const GUIHistory({super.key});
 
   @override
   State<StatefulWidget> createState() => GUIHistoryState();
-
 }
 
-class GUIHistoryState extends State{
-
+class GUIHistoryState extends State {
   @override
   Widget build(BuildContext context) {
-      return Container();
+    return Container();
   }
 }
---
+++ lib/Services/metamask_service.dart
@@ -0,0 +1,18 @@
+import 'package:web3dart/web3dart.dart';
+
+class MetamaskService {
+  final EthereumAddress _defaultAddress;
+
+  MetamaskService(this._defaultAddress);
+
+  // Future<BigInt> getBalance() async {
+  //   // Method to fetch account balance from Metamask
+  // }
+
+  // Future<TransactionHash> sendTransaction(
+  //   EthereumAddress recipient,
+  //   BigInt amount,
+  // ) async {
+  //   // Method to send transaction via Metamask
+  // }
+}
